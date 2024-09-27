243041e0d5dcf9f2852ba7f69a18fe2bbf1459b6
Saima <saimaalam.dsi@gmail.com>
2024-09-27 14:15:44 +0600

    init

--- src/test/java/ElementPosition.java
+++ src/test/java/ElementPosition.java
@@ -1,8 +1,5 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Rectangle;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeMethod;
--- src/test/java/Screenshot.java
+++ src/test/java/Screenshot.java
@@ -1,8 +1,10 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.io.FileHandler;
 import org.testng.annotations.BeforeMethod;
@@ -25,8 +27,22 @@ public class Screenshot {
     public void takeWebelementScreenshot() throws IOException {
         WebElement header= driver.findElement(By.cssSelector("#h-ai-powered-end-to-end-testing"));
         File source = header.getScreenshotAs(OutputType.FILE);
-        File destination = new File("header.png");
+        File destination = new File("screenshots/header.png");
         FileHandler.copy(source, destination);
+    }
+    @Test
+    public void takeWebElementPageSectionScreenshot() throws IOException {
+        WebElement applitoolsPageSection = driver.findElement
+                (By.xpath("//*[@id='post-8']/div[1]/div/div"));
+        File source = applitoolsPageSection.getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(source, new File("screenshots/Applitools Page Section.png"));
+
+    }
+    @Test
+    public void takeFullPageScreenshot() throws IOException {
+        File source = ((FirefoxDriver)driver).getFullPageScreenshotAs(OutputType.FILE);
+        File destination = new File("screenshots/Full Page Screenshot.png");
+        FileHandler.copy(source,destination);
     }
 
 
